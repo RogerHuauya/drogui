6a94f4a9c3e1006f918fa3ae7113d5e5b8b4b88c
GuillermoInglis <ginglisv@uni.pe>
2020-06-12 12:56:49 -0500

    Coppelia matlab binding

---
+++ matlab/initCoppelia.m
@@ -0,0 +1,9 @@
+
+disp('Program started');
+sim = remApi('remoteApi'); % using the prototype file (remoteApiProto.m)
+sim.simxFinish(-1); % just in case, close all opened connections
+clientID=sim.simxStart('127.0.0.1',19999,true,true,5000,5);
+
+if(clientID>-1)
+    disp('Connected to remote API server');
+end
