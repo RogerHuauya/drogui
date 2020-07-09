886405f7ddd9e640b1e87dc9e3ae99128fbf2630
GuillermoInglis <ginglisv@uni.pe>
2020-07-08 23:21:09 -0500

    Mega Update
    Co-authored-by: Roger Gustavo Huauya Mamani <rhuauyam@uni.pe>

--- iot/receiveCamera.cpp
+++ iot/receiveCamera.cpp
@@ -7,29 +7,31 @@
 #include <unistd.h> 
 #include <string.h>
 using namespace cv;
-#define HEIGHT 50
-#define WIDTH 50
+#define HEIGHT 480
+#define WIDTH 640
 #define BUFF_LENGTH (HEIGHT*WIDTH)
 #define ip  "190.232.68.139"
-#define port 8888 
+#define port 8889 
 
 void buffToMat(Mat* im, unsigned char buff[]){
-    uchar* p;
-    for (int i = 0; i < im->rows; ++i) {
-        p = im->ptr<uchar>(i);
-        for (int j = 0; j < im->cols; ++j) {
-            p[j] = buff[i * im -> cols + j];
+	uchar* p;
+	for (int i = 0; i < im->rows; ++i) {
+		p = im->ptr<uchar>(i);
+		for (int j = 0; j < im->cols; ++j) {
+			p[j] = buff[i * im -> cols + j];
         }
     }
 }
 
-int main(int argc, char** argv )
-{
+
+int main(int argc, char** argv ){
+
     int sock = 0, valread; 
 	struct sockaddr_in serv_addr;
+	unsigned char buffer[BUFF_LENGTH] = {0}; 
+    Mat image(HEIGHT, WIDTH, CV_8UC1);
 
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-	{ 
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){ 
 		printf("\n Socket creation error \n"); 
 		return -1; 
 	} 
@@ -37,41 +39,27 @@ int main(int argc, char** argv )
 	serv_addr.sin_family = AF_INET; 
 	serv_addr.sin_port = htons(port); 
 
-	if(inet_pton(AF_INET,ip, &serv_addr.sin_addr)<=0) 
-	{ 
+	if(inet_pton(AF_INET,ip, &serv_addr.sin_addr)<=0){ 
 		printf("\nInvalid address/ Address not supported \n"); 
 		return -1; 
 	} 
 
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-	{ 
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){ 
 		printf("\nConnection Failed \n"); 
 		return -1; 
 	} 
-	
-    
 
-	unsigned char buffer[BUFF_LENGTH] = {0}; 
-    Mat image(HEIGHT, WIDTH, CV_8UC1);
-
-    //namedWindow("Display Image", WINDOW_AUTOSIZE );
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
     
     while(1){   
-        printf("Waiting for message\n");
+		printf("Waiting for message\n");
 		valread = 0;
 		while(valread < BUFF_LENGTH){
-        	valread += read(sock , buffer, BUFF_LENGTH - valread);
-			printf("%d\n", valread);
+        	valread += read(sock , buffer + valread, BUFF_LENGTH - valread);
 		} 
-        printf("Buffer received: \n%d\n", valread);
 		buffToMat(&image, buffer);
-
-        printf("Image processed\n");
-
-
-        imshow("Display Image", image);
-		if(waitKey(5) >= 0) break;
+		imshow("Display Image", image);
+		waitKey(1);
     }
     return 0;
-
-}
+} 
---
+++ iot/src/sockets.cpp
@@ -0,0 +1,84 @@
+#include "sockets.h"
+
+int Socket::Connect(){
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0){ 
+        perror("\nConnection Failed \n"); 
+        return -1; 
+    } 
+    return 0;
+}
+
+int Socket::Create(){
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){ 
+        perror("\n Socket creation error \n"); 
+        return -1; 
+    }
+    return 0;
+}
+
+int Socket::Address(){
+    if(inet_pton(AF_INET,ip, &addr.sin_addr)<=0){ 
+        perror("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+    return 0;
+}
+
+int Socket::Attach(){
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){ 
+		perror("setsockopt"); 
+		return -1;
+	} 
+    return 0;
+}
+
+int Socket::Bind(){
+    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr))<0) { 
+		perror("bind failed"); 
+		return -1;
+	} 
+    return 0;
+}
+
+int Socket::Listen(){
+    if (listen(sock, 3) < 0) { 
+		perror("listen"); 
+		return -1; 
+	} 
+    return 0;
+}
+
+int Socket::Accept(){
+	if ((child_socket = accept(child_socket, (struct sockaddr *)&addr, (socklen_t *)&addrlen))<0) { 
+		perror("accept "); 
+        return -1;
+	} 
+    return 0;
+}
+
+int Socket::clientStart(){
+    if(Create() < 0)    return -1;
+    if(Address() < 0)   return -2;
+    if(Connect() <0)    return -3;
+    return 0;
+}
+
+int Socket::serverStart(){
+    if(Create() < 0)    return -1;
+    if(Attach() < 0)    return -2;
+    if(Bind() < 0)      return -3;
+    if(Listen() < 0)    return -4;
+    if(Accept() < 0)    return -5;
+    return 0;
+}
+
+void Socket::readBuffer(unsigned char buff[], int len){
+    valread = 0;
+    while(valread < len){
+        valread += read(sock , buff + valread, len - valread);
+    } 
+}
+
+void Socket::sendBuffer(unsigned char buff[], int len){
+    send(child_socket , buff , len, 0); 	
+}
