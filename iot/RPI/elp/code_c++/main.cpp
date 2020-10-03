157dd35d4711ee2eca32b6c2f9169f11f7718a00
CristianLazoQuispe <clazoq@uni.pe>
2020-10-03 12:06:47 -0500

    tmr

--- iot/RPI/elp/code_c++/main.cpp
+++ iot/RPI/elp/code_c++/main.cpp
@@ -6,7 +6,8 @@ utils::FPS fps;
 // rm -r results*
 
 int main(){
-    int id,width=1280,height=480;
+    //int id,width=1280,height=480;
+    int id,width=640,height=240;
     std::cout<<"id ";//width height";
     std::cin>>id;//>>width>>height;    
     elp elp1(id,width,height);
