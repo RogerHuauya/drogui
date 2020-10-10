cc89c016896aa46f90aa6a9ee3fb4564c2685986
Cristian Lazo Quispe <mecatronico.lazo@gmail.com>
2020-10-10 18:28:33 -0500

    nuevo

--- raspberry/src/utils.cpp
+++ raspberry/src/utils.cpp
@@ -1,25 +1,25 @@
 #include "utils.h"
 
-void int32pack(unsigned char *bytesint32,int32_t &n){
-    n = int((unsigned char)(bytesint32[0]) << 24 |
-            (unsigned char)(bytesint32[1]) << 16 |
-            (unsigned char)(bytesint32[2]) << 8 |
-            (unsigned char)(bytesint32[3]));
+void int32pack(uint8_t *bytesint32,int32_t *n){
+    *n = (int32_t)((int32_t)(bytesint32[0]) << 24 |
+            (int32_t)(bytesint32[1]) << 16 |
+            (int32_t)(bytesint32[2]) << 8 |
+            (int32_t)(bytesint32[3]));
 };
-void int32unpack(int32_t n,unsigned char *bytesint32){
+void int32unpack(int32_t n,uint8_t *bytesint32){
     bytesint32[0] = (n >> 24) & 0xFF;
     bytesint32[1] = (n >> 16) & 0xFF;
     bytesint32[2] = (n >> 8) & 0xFF;
     bytesint32[3] =  n & 0xFF;
 };
-void floatunpack(float n,unsigned char *bytesfloat){
+void floatunpack(float n,uint8_t *bytesfloat){
     memcpy(bytesfloat, &n, sizeof n);
 };
-void floatpack(unsigned char *bytesfloat,float &n){
-    memcpy(&n, bytesfloat, sizeof n);
+void floatpack(uint8_t *bytesfloat,float *n){
+    memcpy(n, bytesfloat, sizeof n);
 };
 
-void print4bytes(unsigned char *data){
+void print4bytes(uint8_t *data){
     for (int i = 0; i < 4; ++i){
         std::cout<<+data[i]<<" ";
     }
