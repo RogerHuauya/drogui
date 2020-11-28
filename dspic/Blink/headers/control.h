7c4840e4c545e10b7c5669e0d749ab32942ff1a5
GuillermoInglis <ginglisv@uni.pe>
2020-11-28 17:33:25 -0500

    z test

--- dspic/Blink/headers/control.h
+++ dspic/Blink/headers/control.h
@@ -3,6 +3,9 @@
 
 #include "utils.h"
 
+
+enum PIDconstants {ROLL, PITCH, YAW};
+
 typedef struct _pid{
     double kp[5], kd[5], ki[5];
     double threshold[5];
@@ -15,5 +18,6 @@ double computeIndexedPid(pid* p, double error, unsigned long long t, double h);
 double computePid(pid* p, double error, unsigned long long t);
 void resetPid(pid* p, double ti);
 void initPid(pid* p, double kp, double kd, double ki,double ti,double isat,double osat);
+void initPidConstants();
 
 #endif
--- dspic/Blink/headers/utils.h
+++ dspic/Blink/headers/utils.h
@@ -17,4 +17,7 @@ void floattobytes(float n, uint8_t* bytesfloat);
 float bytestofloat(uint8_t *bytesfloat);
 float getReg(uint8_t reg);
 void setReg(uint8_t reg, float val);
+void getEuler(double q0, double q1, double q2, double q3, double* roll, double *pitch, double* yaw);
+double angle_dif(double angle1, double angle2);
+
 #endif
