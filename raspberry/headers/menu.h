6bb725b9272b8f0866c62ecbb6ac716913f5dcc3
Roger Gustavo Huauya Mamani <38589877+ipatheticoi@users.noreply.github.com>
2020-12-02 10:11:24 -0500

    menu lib created

---
+++ raspberry/headers/menu.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "registerMap.h"
+#include "arduPi.h"
+#include "sim7600.h"
+#include <ctime>
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+
+#include "utils.h"
+#include "control.h"
+#include <pthread.h>
