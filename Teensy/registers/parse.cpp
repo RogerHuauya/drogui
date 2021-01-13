ac7433dea8d12a9f738121d684dafba8bf333a8e
GuillermoInglis <ginglisv@uni.pe>
2021-01-13 10:24:53 -0500

    register parse

---
+++ Teensy/registers/parse.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int reg = 2;
+
+    string entrada, aux, out;
+
+    while(getline(cin, entrada)){
+        if(entrada.size() == 0){ cout << endl; continue; }
+
+        stringstream in(entrada);
+        out = "";
+
+        in >> aux; out += aux + " ";
+        in >> aux; out += aux + "\t\t";
+
+        cout << out;
+        printf("0x%04X", reg);
+        reg += 8;
+        cout << endl;
+        
+    }
+
+
+
+}
