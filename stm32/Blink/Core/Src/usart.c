6a2e7ba6a295f8cbfdcd3d9f06364cc352073f53
Julio César Romero Torres <jcromerot@uni.pe>
2021-04-20 13:40:05 -0500

    Data Gyro, Gyro Delay

--- stm32/Blink/Core/Src/usart.c
+++ stm32/Blink/Core/Src/usart.c
@@ -39,7 +39,12 @@ void MX_USART2_UART_Init(void)
 
   /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 9600;
+  #if PORT  == DEBUG
+    huart2.Init.BaudRate = 2000000;
+  #elif PORT == GPS
+    huart2.Init.BaudRate = 9600;
+  #endif
+  
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
