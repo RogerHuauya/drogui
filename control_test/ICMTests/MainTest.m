11f0b014becd0ac5a28632ec8e84c20d92c72621
Julio César Romero Torres <jcromerot@uni.pe>
2021-05-11 20:43:20 -0500

    Some test gyro replace sg

--- control_test/ICMTests/MainTest.m
+++ control_test/ICMTests/MainTest.m
@@ -1,5 +1,5 @@
 name = dir('*rpy.txt').name;
-datos = appendTest(datos,name, ' 400 30 -50 ', '30 80 1500','0 0 0 ' , '0 0 0');
+datos = appendTest(datos,name, ' 400 30 -10 ', '30 80 1500','0 0 0 ' , '0 0 0');
 T = datos(end).datos;
 delete(name);
 delete(strrep(name, "rpy", "xyz"));
