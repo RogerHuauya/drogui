8afdd32b2518758e8284ec889f19ff523a60eeaa
Roger Gustavo Huauya Mamani <rhuauyam@uni.pe>
2021-05-14 15:02:12 -0500

    optical flow edited arduino

--- Teensy/optical_flow/optical_flow.ino
+++ Teensy/optical_flow/optical_flow.ino
@@ -1,3 +1,26 @@
+uint8_t crc8_dvb_s2(uint8_t crc, uint8_t a){
+    crc ^= a;
+    for (int ii = 0; ii < 8; ++ii) {
+        if (crc & 0x80) {
+            crc = (crc << 1) ^ 0xD5;
+        } else {
+            crc = crc << 1;
+        }
+    }
+    return crc;
+}
+
+uint8_t crc8_func(int len, uint8_t* msg){
+    /*for(int i= 0; i < len; i++) {
+      Serial.print(int(msg[i]), HEX); Serial.print(" ");
+    }
+    Serial.println();*/
+    uint8_t crc8t = 0;
+    for(int i = 0; i < len; i++){
+        crc8t = crc8_dvb_s2(crc8t, msg[i]);
+    }
+    return crc8t;
+}
 void setup() {
   // put your setup code here, to run once:
   Serial1.begin(115000);
@@ -6,8 +29,8 @@ void setup() {
 }
 
 void loop() {
-  /*uint8_t send_buff[] = {'$', 'M', '<', 0, 7};
-  for(int i = 0 ; i < 5 ; i++) Serial1.write(send_buff[i]);*/
+  uint8_t send_buff[] = {'$', 'M', '<', 0, 7};
+  for(int i = 0 ; i < 5 ; i++) Serial1.write(send_buff[i]);
   char comando[50];
   int idx = 0;
   uint8_t q1, q2;
@@ -26,26 +49,22 @@ void loop() {
             
             q1 = comando[8];
             fx = 0, fy = 0;
-            for(int i = 3 ; i >= 0 ; i--)
-              /*Serial.print(int(comando[9+i]), HEX), Serial.print(" "),*/ fx = (fx << 8) | comando[9+i];
+           for(int i = 3 ; i >= 0 ; i--)
+              /*Serial.print(int(comando[9+i]), HEX), Serial.print(" "),*/fx = (fx << 8) | comando[9+i];
               
-            for(int i = 3 ; i >= 0 ; i--)
-              /*Serial.print(int(comando[13+i]), HEX), Serial.print(" "),*/ fy = (fy << 8) | comando[13+i];
+          for(int i = 3 ; i >= 0 ; i--)
+              /*Serial.print(int(comando[13+i]), HEX), Serial.print(" "),*/fy = (fy << 8) | comando[13+i];
             
             /*
             for(int i = 3 ; i < idx-2; i++){
                 Serial.print(int(comando[i]), HEX), Serial.print(" ");
             }
             Serial.println();*/
-            Serial.print(millis()-t);
-            Serial.print("\t");
-            Serial.print(q1);
-            Serial.print("\t");
-  
+            
             Serial.print(fx);
             Serial.print("\t");
             Serial.print(fy);
-            Serial.print("\n");
+            Serial.print("\t");
             t = millis();
           }
           else if(comando[4] == 1){
@@ -54,15 +73,22 @@ void loop() {
               for(int i = 3 ; i >= 0 ; i--)
                   rng = (rng << 8) | comando[9+i];
           
-          }
-          /*Serial.print(q2);
-          Serial.print("\t");
           
-          Serial.print(rng);
-          Serial.print("\t");
+            
+            Serial.print(rng);
+            Serial.print("\t");
+          }
+          /*
+          Serial.print(comando[0]);
+            Serial.print(comando[1]);
+            Serial.print(comando[2]);
+            for(int i = 3; i < idx-2; i++)
+            Serial.print(int(comando[i]), HEX), Serial.print(" ");
+            Serial.print("CRC calc: "); Serial.print(int(crc8_func(idx-6, comando + 3)), HEX);
+            Serial.println();
           */
           
-          
+            Serial.print("\n");
           comando[0] = '$';
           comando[1] = 'X';
           idx = 2;
