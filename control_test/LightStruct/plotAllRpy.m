8e330ae01fa32fe8ab4689ea3968064bcc1b17f2
Julio César Romero Torres <jcromerot@uni.pe>
2021-05-28 17:49:27 -0500

    Some Velx, Vely control test

--- control_test/LightStruct/plotAllRpy.m
+++ control_test/LightStruct/plotAllRpy.m
@@ -3,10 +3,10 @@ figure('Name', "Figure Roll");
 plotHold([Trpy.ROLL_SCURVE, Trpy.ROLL_VAL])
 figure('Name', "Figure Pitch");
 plotHold([Trpy.PITCH_SCURVE, Trpy.PITCH_VAL])
-% figure('Name', "Figure Gyro X");
-% plotHold([Trpy.GYRO_X_REF, Trpy.GYRO_X])
-% figure('Name', "Figure Gyro Y");
-% plotHold([Trpy.GYRO_Y_REF, Trpy.GYRO_Y])
-% 
-% figure('Name', "Figure U");
-% plotHold([Trpy.ROLL_U, Trpy.PITCH_U])
+figure('Name', "Figure Gyro X");
+plotHold([Trpy.GYRO_X_REF, Trpy.GYRO_X])
+figure('Name', "Figure Gyro Y");
+plotHold([Trpy.GYRO_Y_REF, Trpy.GYRO_Y])
+
+figure('Name', "Figure U");
+plotHold([Trpy.ROLL_U, Trpy.PITCH_U])
