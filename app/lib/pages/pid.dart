930ff7c535f387ab20c5ef0526b0de4d62102ac7
Julio César Romero Torres <jcromerot@uni.pe>
2021-06-23 00:17:32 -0500

    Add textfield in XYZ ref page and send 3 values

--- app/lib/pages/pid.dart
+++ app/lib/pages/pid.dart
@@ -2,34 +2,61 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'dart:convert';
 
+class TextFieldWithText extends StatelessWidget {
+  final TextEditingController? control;
+  final String? text;
+  TextFieldWithText({@required this.text, this.control});
 
-class PIDPage extends StatelessWidget{
-	final Socket? sock;
-	PIDPage({@required this.sock});
-	final TextEditingController pidKp = new TextEditingController();
-	void send(String s){
-		sock!.add(utf8.encode(s));
-	}
-	@override
-	Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+        child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(hintText: text!),
+                  controller: control,
+                  keyboardType: TextInputType.number,
+                ),
+                Text(text!),
+              ],
+            )));
+  }
+}
 
-		return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-			children: <Widget>[
-				Text(
-					'PID'
-				),
-				TextField(
-					decoration: InputDecoration(
-						hintText: 'KP'
-					),
-					controller: pidKp,
-					keyboardType : TextInputType.number,
-				),
-				IconButton(onPressed: () => send(pidKp.text), icon: Icon(Icons.send))
+class PIDPage extends StatelessWidget {
+  final Socket? sock;
+  PIDPage({@required this.sock});
+  final TextEditingController pidKp = new TextEditingController();
+  final TextEditingController pidKi = new TextEditingController();
+  final TextEditingController pidKd = new TextEditingController();
 
-			],
-		); 
-	}
+  void send(String s) {
+    sock!.add(utf8.encode(s));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Text('PID'),
+        Row(
+          children: [
+            TextFieldWithText(text: 'kp', control: pidKp),
+            TextFieldWithText(text: 'ki', control: pidKi),
+            TextFieldWithText(text: 'kd', control: pidKd)
+          ],
+        ),
+
+        //TextFieldWithText(text: 'kp', control: pidKp),
+        IconButton(
+            onPressed: () =>
+                send(pidKp.text + " " + pidKi.text + " " + pidKd.text),
+            icon: Icon(Icons.send))
+      ],
+    );
+  }
 }
--- app/lib/pages/setpoint.dart
+++ app/lib/pages/setpoint.dart
@@ -2,34 +2,60 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'dart:convert';
 
+class TextFieldWithText extends StatelessWidget {
+  final TextEditingController? control;
+  final String? text;
+  TextFieldWithText({@required this.text, this.control});
 
-class SetPointPage extends StatelessWidget{
-	final Socket? sock;
-	SetPointPage({@required this.sock});
-	final TextEditingController pidKp = new TextEditingController();
-	void send(String s){
-		sock!.add(utf8.encode(s));
-	}
-	@override
-	Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+        child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(hintText: text!),
+                  controller: control,
+                  keyboardType: TextInputType.number,
+                ),
+                Text(text!),
+              ],
+            )));
+  }
+}
 
-		return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-			children: <Widget>[
-				Text(
-					'Setpoint'
-				),
-				TextField(
-					decoration: InputDecoration(
-						hintText: 'X'
-					),
-					controller: pidKp,
-					keyboardType : TextInputType.number,
-				),
-				IconButton(onPressed: () => send(pidKp.text), icon: Icon(Icons.send))
+class SetPointPage extends StatelessWidget {
+  final Socket? sock;
+  SetPointPage({@required this.sock});
 
-			],
-		); 
-	}
+  final TextEditingController x_ref = new TextEditingController();
+  final TextEditingController y_ref = new TextEditingController();
+  final TextEditingController z_ref = new TextEditingController();
+
+  void send(String s) {
+    sock!.add(utf8.encode(s));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Text('Setpoint'),
+        Row(
+          children: [
+            TextFieldWithText(text: 'X', control: x_ref),
+            TextFieldWithText(text: 'Y', control: y_ref),
+            TextFieldWithText(text: 'Z', control: z_ref)
+          ],
+        ),
+        IconButton(
+            onPressed: () =>
+                send(x_ref.text + " " + y_ref.text + " " + z_ref.text),
+            icon: Icon(Icons.send))
+      ],
+    );
+  }
 }
