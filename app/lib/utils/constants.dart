32db457eccb916fa1a60e93db5ee482f2a3b1a38
GuillermoInglis <ginglisv@uni.pe>
2021-07-02 12:20:29 -0500

    removeing app

--- app/lib/utils/constants.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const MaterialColor primaryColor = Colors.blue;
-const MaterialColor selectedColor = Colors.purple;
-const Duration sentDuration = Duration(milliseconds: 1000);
