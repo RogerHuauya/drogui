15cfb5ec1ab9549d2c2fe72f24baf8c0f7bf018c
GuillermoInglis <ginglisv@uni.pe>
2021-07-02 12:19:33 -0500

    removing app

--- app/lib/utils/utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'constants.dart';
-
-void logg(String message, BuildContext context) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Text(message),
-    duration: sentDuration,
-  ));
-}
