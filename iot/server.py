9924aade63ffddb267bc169adf90a21f5c198cff
Guillermo Inglis Vidal <37117597+GuillermoInglis@users.noreply.github.com>
2021-07-16 18:33:36 -0500

    Client server working  (#16)
    * avances
    
    * python server added.
    
    * chat server created
    
    * server more than 3 clients
    
    * modified ip and port
    
    * dummy client simple
    
    * first app test
    
    * Add textfield in XYZ ref page and send 3 values
    
    * server improved
    
    * App almost done, Protocol created
    
    Some changes in client_simple to adapt to the protocol
    Screens
        * PID
        * Sensors
        * Read and Write
        * SetPoint
        * Options
        * Calibration
    
    * Some changes in drogui_rpi
    
    * client simple changed
    
    * Removing drogui_rpi and adding drogui_global (#14)
    
    * removing app
    
    * server robust
    
    * iot files update
    
    * removed drogui_rpi
    
    Co-authored-by: roger <rhuauyam@uni.pe>
    Co-authored-by: Roger Gustavo Huauya Mamani <38589877+ipatheticoi@users.noreply.github.com>
    Co-authored-by: Julio César Romero Torres <jcromerot@uni.pe>

---
+++ iot/server.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import sys
+import socket
+import selectors
+import types
+
+sel = selectors.DefaultSelector()
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print("Accepted connection from", addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+
+def service_connection(key, mask, events):
+    sock = key.fileobj
+    data = key.data
+#    try:
+    recv_data = b''
+    if mask & selectors.EVENT_READ:
+        try:
+            recv_data = sock.recv(1024)  # Should be ready to read
+        except:
+            pass
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print("closing connection to", data.addr)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        for _key, _mask in events:
+            if _key.data !=None:
+                if _key != key and key.data.outb:
+                    print("echoing", repr(data.outb), "to", _key.data.addr)
+                    sent = _key.fileobj.sendall(data.outb)  # Should be ready to write
+        data.outb = b''           
+ #   except:
+  #      pass
+
+if len(sys.argv) != 3:
+    print("usage:", sys.argv[0], "<host> <port>")
+    sys.exit(1)
+
+host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print("listening on", (host, port))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask, events)
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
