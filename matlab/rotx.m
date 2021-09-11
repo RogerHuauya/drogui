65ce9e60703a14e6eee5c1e6a9445c9cca52063e
Julio César Romero Torres <jcromerot@uni.pe>
2021-09-11 17:46:47 -0500

    sort and clean files in matlab folder

--- matlab/rotx.m
+++
@@ -1,3 +0,0 @@
-function rx = rotx(phi)
-rx = [1 0 0; 0 cos(phi) -sin(phi); 0 sin(phi) cos(phi)];
-end
--- matlab/withoutbias/rpy2R.m
+++
@@ -1,8 +0,0 @@
-function R = rpy2R(roll, pitch, yaw)
-    cr = cos(yaw); cy = cos(roll); cp = cos(pitch);
-    sr = sin(yaw); sy = sin(roll); sp = sin(pitch);
-    
-    R = [cr*cp cr*sp*sy-sr*cy cr*sp*cy+sr*sy;
-        sr*cp sr*sp*sy+cr*cy sr*sp*cy-cr*sy;
-        -sp cp*sy cp*cy];
-end
