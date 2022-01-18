d611daf3b6c312bd0d42edbeb3926d80eb7391a4
Roger Gustavo Huauya Mamani <rhuauyam@uni.pe>
2022-01-18 11:50:14 -0500

    compile file adapted to python global installation

--- matlab/droguiModel/transformedModel/cpp/compile.ps1
+++ matlab/droguiModel/transformedModel/cpp/compile.ps1
@@ -1,4 +1,4 @@
-$PYTHON_PATH = 'C:\\Users\\roger\\AppData\\Local\\Programs\\Python\\Python38'
+$PYTHON_PATH = 'C:\\Program Files\\Python39'
 #g++ main.cpp ss.cpp control.cpp -std=c++11 -I C:\\Users\\roger\\AppData\\Local\\Programs\\Python\\Python38\\include -IC:\\Users\\roger\\AppData\\Local\\Programs\\Python\\Python38\\Lib\\site-packages\\numpy\\core\\include -LC:\\Users\\roger\\AppData\\Local\\Programs\\Python\\Python38\\libs -lpython38
-g++ main.cpp ss.cpp sensors.cpp control.cpp -std=c++11 -I $PYTHON_PATH\\include -I $PYTHON_PATH\\Lib\\site-packages\\numpy\\core\\include -L $PYTHON_PATH\\libs -lpython38
+g++ main.cpp ss.cpp sensors.cpp control.cpp -std=c++11 -I $PYTHON_PATH\\include -I $PYTHON_PATH\\Lib\\site-packages\\numpy\\core\\include -L $PYTHON_PATH\\libs -lpython39
 
